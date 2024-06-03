00bd66d35bb3515fa848854b5b90de4f88fbfa80
ang-iv-iq <165197897+ang-iv-iq@users.noreply.github.com>
2024-06-03 20:03:01 +0200

    JS Ecercise Burger Menu

--- main.js
+++ main.js
@@ -55,3 +55,22 @@ for (const [elena] of deliveryOptionTitle.entries()) {
     checkboxList[elena].classList.toggle("js-checkbox-list");
   });
 }
+
+//Burger Menu
+const burgerMenuBtn = document.querySelector(".burger-menu-btn");
+const burgerMenuBtnClose = document.querySelector(".close-burger-menu-btn");
+const nav = document.querySelector(".nav");
+const body = document.querySelector(".body");
+const overlay = document.querySelector(".overlay");
+
+burgerMenuBtn.addEventListener("click", () => {
+  nav.classList.add("js-burger-menu");
+  overlay.classList.add("js-overlay");
+  body.style.position = "fixed";
+});
+
+burgerMenuBtnClose.addEventListener("click", () => {
+  nav.classList.remove("js-burger-menu");
+  overlay.classList.remove("js-overlay");
+  body.style.position = "relative";
+});
